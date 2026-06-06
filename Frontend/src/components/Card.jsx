60f9ba762315041a5468fad64476cebefa225aa6
Rohit prajapati <rp1485260@gmail.com>
2026-06-06 23:48:35 +0530

    first committed

---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
+  <img src="https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?w=1200" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is some Important text</p>
+    <div className="container w-100">
+      <select className="m-2 h-100 bg-success rounded">
+        {Array.from(Array(6), (e,i)=>{
+          return(
+            <option key={i+1} value={i+1}>{i+1}</option>
+          )
+        })}
+      </select>
+
+      <select className="m-2 h-100 bg-success rounded">
+        <option value="half">Half</option>
+        <option value="full">full</option>
+      </select>
+
+      <div className="d-inline">Total Price</div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Card
